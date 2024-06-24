34d9aea5eb1bba1dfdb90cff33289a2de303a792
Joseph Herrera <josephherreraleon@gmail.com>
2024-06-24 09:26:39 -0600

    Made changes

--- APR/Program.cs
+++ APR/Program.cs
@@ -1,63 +1,35 @@
-﻿decimal amountFinanced = 1000M; // Total que va a pagar
+﻿decimal amountFinanced = 1056M; // Total que va a pagar
 decimal estimatedAPR = 1M; // Estimacion inicial de APR
 decimal payment = 33.61M; // Cantidad de pago al mes
 int numberOfPayments = 36; // Numero de pagos
-decimal estimatedUnitPeriods = 0;
 int periods = 0; //Numero de periodos (meses)
 
 decimal monthRate = 0M; // Estimacion de la tasa al mes
 decimal finalTotalPayment = 0; // Total para la revision final
 
-// Fecha de pago
-DateTime loanDate = new DateTime(2023, 1, 1);
-// Fecha de inicio del préstamo
-DateTime paymentDate = new DateTime(2024, 1, 1);
-
-// Revisar si alguno es el ultimo del mes
-bool isLoanDateLastDay = loanDate == new DateTime(loanDate.Year, loanDate.Month, DateTime.DaysInMonth(loanDate.Year, loanDate.Month));
-bool isPaymentDateLastDay = paymentDate == new DateTime(paymentDate.Year, paymentDate.Month, DateTime.DaysInMonth(paymentDate.Year, paymentDate.Month));
-
-int monthsDifference = ((paymentDate.Year - loanDate.Year) * 12) + paymentDate.Month - loanDate.Month;
-
-// Revisa si ambos son el ultimo dia del mes y si los dias en el mes del pago son menos a los dias en el mes que empezo la deuda para agregar un mes
-if (isLoanDateLastDay && isPaymentDateLastDay && DateTime.DaysInMonth(paymentDate.Year, paymentDate.Month) < DateTime.DaysInMonth(loanDate.Year, loanDate.Month))
-{
-    monthsDifference++;
-}
-
-string[] typeOfDuration = new string[] { "monthly", "multiplesOfMonth", "semiMonthly", "actualDays" };
+string[] typeOfDuration = new string[] { "monthly", "semiMonthly", "weekly", "biweekly" };
 
 string durationSelection = typeOfDuration[0];
 
 switch (durationSelection)
 {
     case "monthly":
-        //periods = monthsDifference;
         periods = 12;
         break;
-    case "multiplesOfMonth":
-        periods = (int)Math.Floor(monthsDifference / estimatedUnitPeriods);
-        break;
     case "semiMonthly":
-        periods = 24;
-        //decimal unitPeriods = monthsDifference * 2;
-        //decimal oddDays = unitPeriods % 7;
-        //while (oddDays >= 15)
-        //{
-        //    oddDays -= 15;
-        //    unitPeriods++;
-        //}
-        //periods = (int)unitPeriods;
+        periods = 24;        
         break;
-    case "actualDays":
-        TimeSpan duration = paymentDate - loanDate;
-        int totalDays = duration.Days;
-        periods = (int)Math.Floor(totalDays / estimatedUnitPeriods);
+    case "weekly":
+        periods = 52;
+        break;
+    case "biweekly":
+        periods = 26;
         break;
     default:
         Console.WriteLine("Unknown duration type");
         break;
 }
+
 // Sumar 0.1 a la estimacion mensual
 decimal Addrate()
 {
